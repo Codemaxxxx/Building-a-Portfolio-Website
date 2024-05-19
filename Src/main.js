0ce531b18010c8b3e448a1a7c4d5c3ba555d9b5a
main <deramartinswill@gmail.com>
2024-05-19 18:51:32 +0200

    first commit

---
+++ Src/main.js
@@ -0,0 +1,17 @@
+import '../Styles/modern-normalize.css'
+import '../Styles/style.css'
+import '../Styles/componenets/header.css'
+import '../Styles/componenets/hero.css'
+import '../Styles/componenets/about.css'
+import '../Styles/componenets/work.css'
+import '../Styles/componenets/kontakt.css'
+import '../Styles/componenets/mobile-nav.css'
+import '../Styles/utils.css'
+
+
+import mobileNav from './utils/mobile-nav'
+import darkMode from './utils/dark.mode'
+
+mobileNav();
+darkMode();
+
